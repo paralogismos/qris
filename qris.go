64839c93e2c52dba38fe9fd050d5e47bbef677aa
David Bowling <mailbowling@gmail.com>
2022-07-18 13:19:30 -0500

    Changed C1 citation tag
    Changed to AB, per Jack's request.

--- qris.go
+++ qris.go
@@ -281,7 +281,7 @@ func WriteQuotes(pf *ParsedFile, fname string) {
 	for _, q := range pf.Quotes {
 		fmt.Fprintln(file, "TY  - ABST")
 		fmt.Fprintln(file, "ID  -", id)
-		fmt.Fprintln(file, "C1  -", cit)
+		fmt.Fprintln(file, "AB  -", cit)
 		fmt.Fprintln(file, "A1  -", name)
 		fmt.Fprintln(file, "Y1  -", year)
 		fmt.Fprintln(file, "AB  -", q.Body)
