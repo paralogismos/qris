81ceab790421d3664019020078e26a070f2f4b2f
David Bowling <mailbowling@gmail.com>
2025-04-27 15:54:27 -0500

    updated tests
    Updated the tests to match the updated program.
    
    Modified `TestParseCitation` to test against the full citation name.
    Previously `ParseCitation` only extracted LASTNAME, FIRSTNAME.
    
    Modified `TestParseQuotes`.
    Changed the examples in `wantQuotes` since there is no longer any need
    to test against the `Supp` or `Note` fields here.
    
    Also modified the testing loop in `TestParseQuotes`: found that I needed to
    trim whitespace from parsed page numbers. I didn't need to do this before;
    I'm not sure what changed that caused this, but getting more methodical with
    how and when whitespace gets trimmed is on my TODO list.
    
    Restructured the conditional checks in the testing loop to make failure causes
    more explicit.
    
    Removed uneccesary checks from the testing loop.
    
    Modified

--- qris_test.go
+++ qris_test.go
@@ -3,6 +3,7 @@
 package qris
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -11,7 +12,7 @@ func TestParseCitation(t *testing.T) {
 		`Brown, Jason W. "Neuropsychology and the self-concept." `+
 			`The Journal of Nervous and Mental Disease. 187 no.3 (1999e): 131-41.`)
 	cit := parseCitation(ex_citation)
-	want_name := "Brown, Jason"
+	want_name := "Brown, Jason W."
 	want_year := "1999e"
 	want_body := `Brown, Jason W. "Neuropsychology and the self-concept." ` +
 		`The Journal of Nervous and Mental Disease. 187 no.3 (1999e): 131-41.`
@@ -45,59 +46,41 @@ func TestParseQuote(t *testing.T) {
 
 	wantQuotes := []Quote{
 		Quote{
-			LineNo: 0,
-			Body:   `Focal attention is focal perception, not perception focused by attention.`,
-			Page:   `3`,
-			Supp:   "",
-			Note:   ""},
+			Body: `Focal attention is focal perception, not perception focused by attention.`,
+			Page: `3`,
+		},
 		Quote{
-			LineNo: 1,
-			Body:   `Self and world are not separate entities that interact, but relations in a continuum of transformation. The world is a product of the same process that lays down the self.`,
-			Page:   `4`,
-			Supp:   "",
-			Note:   ""},
+			Body: `Self and world are not separate entities that interact, but relations in a continuum of transformation. The world is a product of the same process that lays down the self.`,
+			Page: `4`,
+		},
 		Quote{
-			LineNo: 2,
-			Body:   `... [W]e conclude that the belief in a real world does not depend on the realness of the perception but on the coherence of perceptions within and across the different modalities.`,
-			Page:   `5`,
-			Supp:   `w^activity_pattern`,
-			Note:   ""},
+			Body: `... [W]e conclude that the belief in a real world does not depend on the realness of the perception but on the coherence of perceptions within and across the different modalities.`,
+			Page: `5`,
+		},
 		Quote{
-			LineNo: 3,
-			Body:   `The self in a semantic state has a different quality of consciousness from the self in a phonological state. The pathological material helps to dispose of the notion that the self is a substance and mental states are its properties. The properties of the self are states of its existence, not changing features of a self that is unchanging.`,
-			Page:   `7`,
-			Supp:   "",
-			Note:   ""},
+			Body: `The self in a semantic state has a different quality of consciousness from the self in a phonological state. The pathological material helps to dispose of the notion that the self is a substance and mental states are its properties. The properties of the self are states of its existence, not changing features of a self that is unchanging.`,
+			Page: `7`,
+		},
 		Quote{
-			LineNo: 4,
-			Body:   `Contrastive features are important. Contrast involves relations which implicate temporal process. In the relation of figure to ground, there is a microtemporal transition in the kinetics of synchronicity that carves local unities out of the simultaneity of the whole.`,
-			Page:   `7`,
-			Supp:   "",
-			Note:   ""},
+			Body: `Contrastive features are important. Contrast involves relations which implicate temporal process. In the relation of figure to ground, there is a microtemporal transition in the kinetics of synchronicity that carves local unities out of the simultaneity of the whole.`,
+			Page: `7`,
+		},
 		Quote{
-			LineNo: 5,
-			Body:   `[S]elf, image, and world are a single object in transition.`,
-			Page:   `8`,
-			Supp:   `epigraph`,
-			Note:   ""},
+			Body: `[S]elf, image, and world are a single object in transition.`,
+			Page: `8`,
+		},
 		Quote{
-			LineNo: 6,
-			Body:   `The duration of the present creates a theater for experience, but does not fully explain the unity of the self. We need also to explore the nature of categories and the analogy of a duration and its instants to a category and its members. A duration is a "container" of temporal parts; a category is a "container" of spatial parts. A duration is a container of arbitrary length that enfolds a number of instants; a category is a container of an arbitrary set of members. A category is like a duration in that both are enclosures with fuzzy boundaries for virtual parts that are, themselves, potential containers. An instant is like a member of a category in that it can be a category for another member. Duration is the primordial manifestation of categorization.`,
-			Page:   `8`,
-			Supp:   `(notice: according to Brown, time is primordial)  --jmr`,
-			Note:   ""},
+			Body: `The duration of the present creates a theater for experience, but does not fully explain the unity of the self. We need also to explore the nature of categories and the analogy of a duration and its instants to a category and its members. A duration is a "container" of temporal parts; a category is a "container" of spatial parts. A duration is a container of arbitrary length that enfolds a number of instants; a category is a container of an arbitrary set of members. A category is like a duration in that both are enclosures with fuzzy boundaries for virtual parts that are, themselves, potential containers. An instant is like a member of a category in that it can be a category for another member. Duration is the primordial manifestation of categorization.`,
+			Page: `8`,
+		},
 		Quote{
-			LineNo: 7,
-			Body:   `The relation of succession in a nontemporal (vertical) becoming transforms to one of precedence in the serialization of the (horizontal) present by events. The nontemporal actualization precipitates cotemporal entities. The concurrence of these entities in the specious present is consciousness, i.e., the conscious state. The self forms the past boundary, the surface images and objects form the actual boundary, of this duration.`,
-			Page:   `9`,
-			Supp:   `space precipitates simultaneous objects which equals consciousness -jmr`,
-			Note:   ""},
+			Body: `The relation of succession in a nontemporal (vertical) becoming transforms to one of precedence in the serialization of the (horizontal) present by events. The nontemporal actualization precipitates cotemporal entities. The concurrence of these entities in the specious present is consciousness, i.e., the conscious state. The self forms the past boundary, the surface images and objects form the actual boundary, of this duration.`,
+			Page: `9`,
+		},
 		Quote{
-			LineNo: 8,
-			Body:   `Finally, to sum up what this signifies for the biological basis of the self, the neuropsychological material demonstrates that the self is deposited in the process of object realization, that it distributes into images and objects, and that a truncation of this process results in an erosion of the self that is similar across the different perceptual modalities. The self is categorical and relational, achieving autonomy in the context of a complete derivation. The autonomy depends on the completeness. The preliminary locus of the self in the mental state entails a holistic or multimodal phase of potential prior to perceptual individuation. This, together with a relation to feeling, to the personal history and the immediate past, point to a limbic transition in the outward development of the mental state.`,
-			Page:   `14`,
-			Supp:   `penultimate paragraph`,
-			Note:   ""},
+			Body: `Finally, to sum up what this signifies for the biological basis of the self, the neuropsychological material demonstrates that the self is deposited in the process of object realization, that it distributes into images and objects, and that a truncation of this process results in an erosion of the self that is similar across the different perceptual modalities. The self is categorical and relational, achieving autonomy in the context of a complete derivation. The autonomy depends on the completeness. The preliminary locus of the self in the mental state entails a holistic or multimodal phase of potential prior to perceptual individuation. This, together with a relation to feeling, to the personal history and the immediate past, point to a limbic transition in the outward development of the mental state.`,
+			Page: `14`,
+		},
 	}
 
 	var exQuoteLines []Line
@@ -107,23 +90,23 @@ func TestParseQuote(t *testing.T) {
 
 	for n, ql := range exQuoteLines {
 		q, isQuote := parseQuote(ql)
+		q.Body = strings.TrimSpace(q.Body)
+		q.Page = strings.TrimSpace(q.Page)
 		want := wantQuotes[n]
-		if !isQuote ||
-			q.Body != want.Body ||
-			q.Page != want.Page ||
-			q.Supp != want.Supp ||
-			q.Note != want.Note {
-			t.Errorf("parseQuote(<exTestLines[%d]>)\n"+
-				"Page = %s\n want: %s\n"+
-				"Supp = %s\n want: %s\n"+
-				"Note = %s\n want: %s\n\n"+
+		if !isQuote {
+			t.Logf("unable to parse <exTestLines[%d]\n", n)
+			t.FailNow()
+		}
+		if q.Body != want.Body {
+			t.Errorf("failure in Body of <exTestLines[%d]>\n"+
 				"Body = %s\n\n"+
 				" want: %s",
-				n,
-				q.Page, want.Page,
-				q.Supp, want.Supp,
-				q.Note, want.Note,
-				q.Body, want.Body)
+				n, q.Body, want.Body)
+		}
+		if q.Page != want.Page {
+			t.Errorf("failure in Page of <exTestLines[%d]>\n"+
+				"Page = %s\n want: %s\n",
+				n, q.Page, want.Page)
 		}
 	}
 }
