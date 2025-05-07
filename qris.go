86bf1be564f3512b6b02ceaefa28776481bc4b4e
David Bowling <mailbowling@gmail.com>
2025-05-07 15:17:30 -0500

    incremented version number to v0.14.0

--- qris.go
+++ qris.go
@@ -135,7 +135,7 @@ import (
 )
 
 // Definitions of system constants.
-const Version = "v0.13.0"
+const Version = "v0.14.0"
 const parsedSuffix = "_PARSED.ris"
 const discardSuffix = "_DISCARD.txt"
 const configDir = "qris"
