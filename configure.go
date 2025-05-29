27d85b1b89284a169b642fe01c7b1d78a3233088
David Bowling <mailbowling@gmail.com>
2025-05-28 19:46:19 -0500

    incremented version number to v0.19.2

--- configure.go
+++ configure.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Definitions of system constants.
-const Version = "v0.19.1"
+const Version = "v0.19.2"
 const parsedSuffix = "_PARSED.ris"
 const discardSuffix = "_DISCARD.txt"
 const configDir = "qris"
